9d7bbfa0ec98c07f5ff2e9f4fc297c8984746a34
juan <juan.gutierrez12@uptc.edu.co>
2024-07-12 21:20:50 -0500

    Creacion de login, registro e  inicio

--- web_app/models.py
+++ web_app/models.py
@@ -6,19 +6,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    profiles = db.relationship('UserProfile', backref='user', lazy=True)
-    requests = db.relationship('UserRequest', backref='user', lazy=True)
 
 class UserProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
+    user = db.relationship('User', backref=db.backref('profile', lazy=True))
+    email = db.Column(db.String(120), unique=True, nullable=False)
     birthdate = db.Column(db.Date, nullable=False)
-    gender = db.Column(db.String(20), nullable=False)
-    interests = db.Column(db.String(500), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    interests = db.Column(db.String(250), nullable=True)
 
 class UserRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     request_text = db.Column(db.Text, nullable=False)
-    response_text = db.Column(db.Text, nullable=True)  # Asegúrate de que esta columna esté definida
+    response_text = db.Column(db.Text, nullable=True)
+    user = db.relationship('User', backref=db.backref('requests', lazy=True))
